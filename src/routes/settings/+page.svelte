5fc32514fcc524c238ed54dc8430af2f8be3f677
Tuomas Rinne <tuomas.rin@hotmail.com>
2022-08-31 19:15:44 +0300

    style toggle

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -4,15 +4,17 @@
 
 <div class="flex flex-grow flex-col">
 	<div class="flex justify-evenly flex-col">
-		<div class="flex flex-row justify-around">
+		<div class="flex justify-center">!!These don't work yet!!</div>
+
+		<div class="flex flex-row justify-around m-1">
 			<p>text color</p>
 			<Toggle />
 		</div>
-		<div class="flex flex-row justify-around">
+		<div class="flex flex-row justify-around m-1">
 			<p>background color</p>
 			<Toggle />
 		</div>
-		<div class="flex flex-row justify-around">
+		<div class="flex flex-row justify-around m-1">
 			<p>crt effect</p>
 			<Toggle />
 		</div>
