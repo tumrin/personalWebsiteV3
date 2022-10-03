4c07c06a1ef27a1448d03d78d9813a6e9bbbf076
Tuomas Rinne <tuomas.rin@hotmail.com>
2022-10-03 16:19:40 +0300

    fix mobile view

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 	import Topbar from '../components/topbar.svelte';
 </script>
 
-<div class="crt">
+<div class="crt overflow-clip">
 	<div class="text-white text-shadow font-mono flex flex-col h-screen">
 		<Topbar />
 		<div class="flex flex-grow justify-start align-middle m-8 overflow-y-auto overflow-x-clip">
