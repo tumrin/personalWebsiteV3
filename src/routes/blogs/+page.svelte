7c1a66ba6379b97605b10881f886e63e495b5c85
Tuomas Rinne <tuomas.rin@hotmail.com>
2025-12-19 10:25:08 +0200

    add blog

--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -5,6 +5,11 @@
 <div class="blogs">
 	<h1>Personal Blogs</h1>
 	<h1>Professional Blogs</h1>
+	<Blog
+		name="Technology Choices in Software Projects and Tools"
+		language="Finnish"
+		url="https://kvanttori.fi/blogi/teknologiavalinnat"
+	/>
 	<Blog name="Unsafe Rust" language="Finnish" url="https://kvanttori.fi/blogi/unsafe-rust" />
 	<Blog
 		name="Introduction to Energy Efficiency of Software"
