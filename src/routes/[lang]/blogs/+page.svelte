d555522458f93d752439f2a12d22a43f1edaac85
Tuomas Rinne <tuomas.rin@hotmail.com>
2026-02-12 08:53:37 +0200

    update blog styling

--- src/routes/[lang]/blogs/+page.svelte
+++ src/routes/[lang]/blogs/+page.svelte
@@ -8,26 +8,31 @@
 	<meta name="description" content={$LL.blogs.description()} />
 </svelte:head>
 
-<div class="blogs">
-	<Blog
-		name="European Services and Digital Sovereignty"
-		url="https://kvanttori.fi/en/blogi/eurooppalaiset-palvelut"
-	/>
-	<Blog
-		name="Technology Choices in Software Projects and Tools"
-		url="https://kvanttori.fi/en/blogi/teknologiavalinnat"
-	/>
-	<Blog name="Unsafe Rust" url="https://kvanttori.fi/en/blogi/unsafe-rust" />
-	<Blog
-		name="Introduction to Energy Efficiency of Software"
-		url=" https://kvanttori.fi/en/blogi/johdatus-ohjelmien-energiatehokkuuteen"
-	/>
+<div class="blogspage">
+	<div class="blogs">
+		<Blog
+			name="European Services and Digital Sovereignty"
+			url="https://kvanttori.fi/en/blogi/eurooppalaiset-palvelut"
+			year={2025}
+		/>
+		<Blog
+			name="Technology Choices in Software Projects and Tools"
+			url="https://kvanttori.fi/en/blogi/teknologiavalinnat"
+			year={2025}
+		/>
+		<Blog name="Unsafe Rust" url="https://kvanttori.fi/en/blogi/unsafe-rust" year={2024} />
+		<Blog
+			name="Introduction to Energy Efficiency of Software"
+			url=" https://kvanttori.fi/en/blogi/johdatus-ohjelmien-energiatehokkuuteen"
+			year={2023}
+		/>
+	</div>
 </div>
 
 <style>
 	.blogs {
 		display: flex;
 		flex-direction: column;
-		gap: 1rem;
+		gap: 1.5rem;
 	}
 </style>
--- src/routes/[lang]/blogs/blog.svelte
+++ src/routes/[lang]/blogs/blog.svelte
@@ -1,14 +1,39 @@
 <script lang="ts">
 	export let name: string;
 	export let url: string;
+	export let year: number;
 </script>
 
-<a class="blogEntry" href={url}>{name}</a>
+<a class="blogEntry" href={url} target="_blank" rel="noopener noreferrer">
+	<span class="title"><span class="year">{year}</span> - {name}</span>
+</a>
 
 <style>
 	.blogEntry {
+		display: flex;
+		padding: 1rem;
 		background-color: #3b4252;
-		padding: 2rem;
+		font-size: 1rem;
+		font-weight: bolder;
 		border-radius: 5px;
+		color: white;
+		cursor: pointer;
+		text-decoration: none;
+	}
+
+	.blogEntry:hover {
+		background-color: #434c5e;
+	}
+
+	.title {
+		text-align: left;
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+
+	.year {
+		font-weight: bold;
+		color: #88c0d0;
 	}
 </style>
