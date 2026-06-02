882196379e17912be857b6568a21b7989203d543
Tuomas Rinne <tuomas.rin@protonmail.com>
2026-06-02 22:25:45 +0300

    add codeberg

--- src/lib/components/socials.svelte
+++ src/lib/components/socials.svelte
@@ -1,16 +1,26 @@
 <script lang="ts">
 	import Fa from 'svelte-fa';
+	import Codeberg from './codeberg-logo_icon_white.svelte';
 	import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-	const color = '#88c0d0';
+	const color = '#FFFFFF';
 </script>
 
 <div class="socials">
-	<a href="https://github.com/tumrin" aria-label="Link to GitHub">
-		<Fa size="2x" {color} icon={faGithub} />
-	</a>
-	<a href="https://www.linkedin.com/in/tuomas-rinne-0840a6157/" aria-label="Link to LinkedIn">
-		<Fa size="2x" {color} icon={faLinkedin} />
-	</a>
+	<div class="social">
+		<a href="https://codeberg.org/tumrin" aria-label="Link to Codeberg">
+			<Codeberg />
+		</a>
+	</div>
+	<div class="social">
+		<a href="https://github.com/tumrin" aria-label="Link to GitHub">
+			<Fa scale={2} {color} icon={faGithub} />
+		</a>
+	</div>
+	<div class="social">
+		<a href="https://www.linkedin.com/in/tuomas-rinne-0840a6157/" aria-label="Link to LinkedIn">
+			<Fa scale={2} {color} icon={faLinkedin} />
+		</a>
+	</div>
 </div>
 
 <style>
@@ -20,5 +30,12 @@
 		align-items: center;
 		gap: 2rem;
 		height: 10%;
+	}
+	.social {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 32px;
+		height: 32px;
 	}
 </style>
